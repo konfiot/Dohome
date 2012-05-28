fcd125fa3a094e36ca955df16b0c3d89d2ba50a1
Arthur <arthurtoussaint@free.fr>
2012-05-28 09:38:51 +0200

    debogage du script de MAJ

--- Scripts MAJ/zip.py
+++ Scripts MAJ/zip.py
@@ -3,8 +3,12 @@ import sys
 import zipfile
 
 def visit (arg, dirname, files):
-	zipf = zipfile.ZipFile(arg[0], "w")
-	for i in files :
-		zipf.write(dirname.replace(arg[1], "") + i)
+        for i in files :
+                if (not(os.path.isdir(dirname.replace(arg[1], "") + i))) and (i != "vars.js"):
+                        print dirname.replace(arg[1], "") + "\\" + i
+                        zipf.write(dirname+ "\\" + i, dirname.replace(arg[1], "") + "\\" + i)
 
-os.path.walk(sys.argv[1].replace("temp", "Client"), visit, [sys.argv[1] + "/dohome.zip", sys.argv[1].replace("temp", "Client")])
+arg = [sys.argv[1] + "\\dohome.zip", sys.argv[1].replace("temp", "Client")]
+zipf = zipfile.ZipFile(arg[0], "w")
+os.path.walk(sys.argv[1].replace("temp", "Client"), visit, arg)
+zipf.close();
